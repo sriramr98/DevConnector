56ca6352fe3d00926ff836a81d7abc121b5454e7
Sriram R <sriramr083@gmail.com>
2019-01-09 14:43:11 +0530

    added Header,Footer and Landing page templates with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Welcome to Dev Connector</h1>
-            </div>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <Footer />
+                </div>
+            </Router>
         );
     }
 }
