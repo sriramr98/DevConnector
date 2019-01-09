3a2698baba2c1a02a488fea0e819d7f92ab046c5
Sriram R <sriramr083@gmail.com>
2019-01-09 14:25:57 +0530

    setup the basic client react packages

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/posts", postsRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
