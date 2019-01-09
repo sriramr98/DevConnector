37ac8b51ee63aa6d4db6aab8e14e4fc54715cf87
Sriram R <sriramr083@gmail.com>
2019-01-09 14:51:10 +0530

    setup react router and basic app routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
     render() {
@@ -12,6 +14,10 @@ class App extends Component {
                 <div className="App">
                     <Navbar />
                     <Route exact path="/" component={Landing} />
+                    <div className="container">
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                    </div>
                     <Footer />
                 </div>
             </Router>
